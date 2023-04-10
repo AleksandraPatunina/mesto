a5d9364cd596778bb0a04b2147010d5bcfd187e4
Aleksandra Patunina <sasha_gym5@mail.ru>
2023-04-10 20:14:33 +0300

    Исправлены ошибки после второго ревью.Отформатирован код

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,33 @@
 //открытие и закрытие popup
-const openPopupBtn = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupBtn = document.querySelector('.popup__button-close');
-let profileName = document.querySelector('.profile__info-title');
-let profileJob = document.querySelector('.profile__info-subtitle');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
-let formElement = document.querySelector('form');
+const openPopupBtn = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closePopupBtn = document.querySelector(".popup__button-close");
+let profileName = document.querySelector(".profile__info-title");
+let profileJob = document.querySelector(".profile__info-subtitle");
+let nameInput = document.querySelector(".popup__input_type_name");
+let jobInput = document.querySelector(".popup__input_type_job");
+let formElement = document.querySelector("form");
 
 //открытие popup
 function openPopup() {
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-    popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add("popup_opened");
 }
 
 //закрытие popup
 function closePopup() {
-    popup.classList.remove('popup_opened');
+  popup.classList.remove("popup_opened");
 }
 
 //сохранение данных из формы
 function handleFormSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    closePopup();
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
 }
 
-openPopupBtn.addEventListener('click', openPopup);
-closePopupBtn.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
-
-
-
-
-
-
-
+openPopupBtn.addEventListener("click", openPopup);
+closePopupBtn.addEventListener("click", closePopup);
+formElement.addEventListener("submit", handleFormSubmit);
