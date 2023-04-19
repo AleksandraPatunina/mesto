bd132984c2a0dd2c89240a4aaefb825e3090b0c6
Aleksandra Patunina <sasha_gym5@mail.ru>
2023-04-19 19:59:00 +0300

    Добавлен в js следующий функционал: лайк для фото, закрытие 2-ч popup, открытие popup с карточками

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
-//открытие и закрытие popup
+//объявление переменных
 const openPopupBtn = document.querySelector(".profile__edit-button");
+const addPopupBtn = document.querySelector(".profile__add-button");
 const popup = document.querySelector(".popup");
-const closePopupBtn = document.querySelector(".popup__button-close");
+const addPopup = document.querySelector(".add-popup");
+const closePopupBtn = document.querySelectorAll(".popup__button-close");
+const likeBtn = document.querySelector(".element__like-button");
 let profileName = document.querySelector(".profile__info-title");
 let profileJob = document.querySelector(".profile__info-subtitle");
 let nameInput = document.querySelector(".popup__input_type_name");
@@ -15,19 +18,36 @@ function openPopup() {
   popup.classList.add("popup_opened");
 }
 
-//закрытие popup
-function closePopup() {
-  popup.classList.remove("popup_opened");
-}
-
 //сохранение данных из формы
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  closePopup();
+  popup.classList.remove("popup_opened");
 }
 
+//открытие popup с карточками
+function openAddPopup() {
+  addPopup.classList.add("popup_opened");
+}
+
+//закрытие всех popup
+closePopupBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    // находим родительский элемент кнопки - popup
+    const popup = button.closest('.popup');
+    popup.classList.remove('popup_opened');
+  });
+});
+
+//поставить лайк
+function like() {
+  likeBtn.classList.add("element__like-button_type_active");
+}
+
+
+
 openPopupBtn.addEventListener("click", openPopup);
-closePopupBtn.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
+addPopupBtn.addEventListener("click", openAddPopup);
+likeBtn.addEventListener("click", like);
